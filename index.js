223a730c8a86ececb0cb21bb1055093fa99f1680
kartikk221 <kartikk221@yahoo.com>
2021-04-09 18:01:43 -0400

    Exposed WebsocketRoute through master export

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const Server = require('./components/server.js');
 const SessionEngine = require('./components/session_engine.js');
+const WebsocketRoute = require('./components/websocketRoute.js');
+
 module.exports = {
     Server: Server,
     SessionEngine: SessionEngine,
+    WebsocketRoute: WebsocketRoute,
 };
