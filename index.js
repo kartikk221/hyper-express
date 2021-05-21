28a008df51ad1698258d1ebfb3c1905079370d4e
kartikk221 <kartikk221@yahoo.com>
2021-05-20 20:55:56 -0400

    Removed WebsocketRoute from index

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const Server = require('./components/server.js');
-const SessionEngine = require('./components/session_engine.js');
-const WebsocketRoute = require('./components/websocketRoute.js');
+const uWebsockets = require('uWebSockets.js');
+const Server = require('./src/components/Server.js');
+const SessionEngine = require('./src/components/session/SessionEngine.js');
 
 module.exports = {
     Server: Server,
     SessionEngine: SessionEngine,
-    WebsocketRoute: WebsocketRoute,
+    compressors: {
+        DISABLED: uWebsockets.DISABLED,
+        SHARED_COMPRESSOR: uWebsockets.SHARED_COMPRESSOR,
+        DEDICATED_COMPRESSOR_3KB: uWebsockets.DEDICATED_COMPRESSOR_3KB,
+        DEDICATED_COMPRESSOR_4KB: uWebsockets.DEDICATED_COMPRESSOR_4KB,
+        DEDICATED_COMPRESSOR_8KB: uWebsockets.DEDICATED_COMPRESSOR_8KB,
+        DEDICATED_COMPRESSOR_16KB: uWebsockets.DEDICATED_COMPRESSOR_16KB,
+        DEDICATED_COMPRESSOR_32KB: uWebsockets.DEDICATED_COMPRESSOR_32KB,
+        DEDICATED_COMPRESSOR_64KB: uWebsockets.DEDICATED_COMPRESSOR_64KB,
+        DEDICATED_COMPRESSOR_128KB: uWebsockets.DEDICATED_COMPRESSOR_128KB,
+        DEDICATED_COMPRESSOR_256KB: uWebsockets.DEDICATED_COMPRESSOR_256KB,
+    },
 };
