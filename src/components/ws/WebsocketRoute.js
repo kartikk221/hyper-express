2145d121111fd3dfaa2df8f65bc0ed2fca0f9ac0
kartikk221 <kartikk221@yahoo.com>
2021-08-22 16:19:29 -0400

    Converted Passthrough handlers to arrow based functions

--- src/components/ws/WebsocketRoute.js
+++ src/components/ws/WebsocketRoute.js
@@ -11,41 +11,34 @@ class WebsocketRoute {
     };
 
     constructor(pattern, options, context) {
-        let reference = this;
-
         // Wrap passed options over default options object
         operators.fill_object(this.#options, options);
 
         // Bind passthrough handlers to allow for manual assignments
         options.open = (ws) => this.#methods.open(ws);
         options.drain = (ws) => this.#methods.drain(ws);
-        options.close = (ws, code, message) => {
-            return reference.#methods.close(
-                ws,
-                code,
-                operators.arr_buff_to_str(message)
-            );
-        };
-        options.message = (ws, message, isBinary) => {
-            return reference.#methods.message(
+        options.close = (ws, code, message) =>
+            this.#methods.close(ws, code, operators.arr_buff_to_str(message));
+
+        // Bind message handler that converts incoming ArrayBuffer message to string
+        options.message = (ws, message, isBinary) =>
+            this.#methods.message(
                 ws,
                 isBinary ? message : operators.arr_buff_to_str(message),
                 isBinary
             );
-        };
 
         // Bind passthrough upgrade handler that utilizes same wrapping as normal routes
         let url_parameters_key = operators.parse_path_params(pattern);
-        options.upgrade = (response, request, socket_context) => {
-            return context._handle_wrapped_request(
+        options.upgrade = (response, request, socket_context) =>
+            context._handle_wrapped_request(
                 request,
                 response,
                 socket_context,
-                reference.#methods.upgrade,
+                this.#methods.upgrade,
                 url_parameters_key,
                 context
             );
-        };
 
         // Bind a route to the underlying uWS instance
         context.uws_instance.ws(pattern, options);
@@ -72,9 +65,7 @@ class WebsocketRoute {
             );
 
         if (typeof handler !== 'function')
-            throw new Error(
-                'HyperExpress: .handle(event, handler) -> handler must be a Function'
-            );
+            throw new Error('HyperExpress: .handle(event, handler) -> handler must be a Function');
 
         this.#methods[event] = handler;
     }
