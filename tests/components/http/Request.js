dfe09f16e89bcff5ca47d4a110306a71532b9c78
kartikk221 <kartikk221@yahoo.com>
2021-08-29 00:12:25 -0400

    Implemented Large Body Content Test

--- tests/components/http/Request.js
+++ tests/components/http/Request.js
@@ -1,14 +1,17 @@
 const root = '../../';
-const { log, assert_log, random_string } = require(root +
-    'scripts/operators.js');
+const { log, assert_log, random_string, async_wait } = require(root + 'scripts/operators.js');
 const { fetch, server } = require(root + 'scripts/configuration.js');
 const { webserver } = require(root + 'setup/webserver.js');
+const crypto = require('crypto');
 const endpoint = '/tests/request/:param1/:param2';
 const base = server.base;
+
+let last_endpoint_body;
 
 // Create Backend HTTP Route
 webserver.any(endpoint, async (request, response) => {
     let text = await request.text();
+    last_endpoint_body = text;
     let json = await request.json();
 
     // Return all possible information about incoming request
@@ -30,9 +33,25 @@ webserver.any(endpoint, async (request, response) => {
     });
 });
 
+function crypto_random(length) {
+    return new Promise((resolve, reject) =>
+        crypto.randomBytes(Math.round(length / 2), (error, buffer) => {
+            if (error) return reject(error);
+            resolve(buffer.toString('hex'));
+        })
+    );
+}
+
 async function test_request_object() {
     // Prepare Test Candidates
     log('REQUEST', 'Testing HyperExpress.Request Object...');
+
+    const body_size = 10 * 1024 * 1024;
+    log(
+        'REQUEST',
+        `Generating A Large ${body_size.toLocaleString()} Characters Size Body To Simulate Large Payload...`
+    );
+
     let group = 'REQUEST';
     let candidate = 'HyperExpress.Request';
     let start_time = Date.now();
@@ -42,7 +61,10 @@ async function test_request_object() {
     let query1 = random_string(10);
     let query2 = random_string(10);
     let query = `?query1=${query1}&query2=${query2}`;
-    let body_test_value = random_string(10);
+    let body_test_value = await crypto_random(body_size);
+    let fetch_body = JSON.stringify({
+        test_value: body_test_value,
+    });
     let header_test_value = random_string(10);
     let header_test_cookie = {
         name: random_string(10),
@@ -59,9 +81,7 @@ async function test_request_object() {
             'content-type': 'application/json',
             cookie: `${header_test_cookie.name}=${header_test_cookie.value}`,
         },
-        body: JSON.stringify({
-            test_value: body_test_value,
-        }),
+        body: fetch_body,
     };
 
     // Perform HTTP Request To Endpoint
@@ -78,11 +98,7 @@ async function test_request_object() {
     assert_log(group, candidate + '.path', () => path === body.path);
 
     // Verify .query
-    assert_log(
-        group,
-        candidate + '.query',
-        () => query.substring(1) === body.query
-    );
+    assert_log(group, candidate + '.query', () => query.substring(1) === body.query);
 
     // Verify .ip
     assert_log(group, candidate + '.ip', () => body.ip === '127.0.0.1');
@@ -91,17 +107,13 @@ async function test_request_object() {
     assert_log(group, candidate + '.proxy_ip', () => body.proxy_ip === '');
 
     // Verify .headers
-    assert_log(
-        group,
-        candidate + '.headers["x-test-value", "cookie", "content-length"]',
-        () => {
-            let headers = body.headers;
-            let value_test = headers['x-test-value'] === header_test_value;
-            let cookie_test = headers.cookie === options.headers.cookie;
-            let content_length_test = +headers['content-length'] === 27;
-            return value_test && cookie_test && content_length_test;
-        }
-    );
+    assert_log(group, candidate + '.headers["x-test-value", "cookie", "content-length"]', () => {
+        let headers = body.headers;
+        let value_test = headers['x-test-value'] === header_test_value;
+        let cookie_test = headers.cookie === options.headers.cookie;
+        let content_length_test = +headers['content-length'] === fetch_body.length;
+        return value_test && cookie_test && content_length_test;
+    });
 
     // Verify .query_parameters
     assert_log(group, candidate + '.path_parameters', () => {
@@ -118,23 +130,12 @@ async function test_request_object() {
     );
 
     // Verify .text()
-    assert_log(
-        group,
-        candidate + '.text()',
-        () => body.body.text === options.body
-    );
+    assert_log(group, candidate + '.text()', () => body.body.text === options.body);
 
     // Verify .json()
-    assert_log(
-        group,
-        candidate + '.json()',
-        () => JSON.stringify(body.body.json) === options.body
-    );
+    assert_log(group, candidate + '.json()', () => JSON.stringify(body.body.json) === options.body);
 
-    log(
-        group,
-        `Finished Testing ${candidate} In ${Date.now() - start_time}ms\n`
-    );
+    log(group, `Finished Testing ${candidate} In ${Date.now() - start_time}ms\n`);
 }
 
 module.exports = {
