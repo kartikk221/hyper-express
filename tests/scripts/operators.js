22511c26316b818923486c5772999cb361688e94
kartikk221 <kartikk221@yahoo.com>
2021-08-29 00:13:19 -0400

    Updated

--- tests/scripts/operators.js
+++ tests/scripts/operators.js
@@ -4,21 +4,17 @@ function log(logger = 'SYSTEM', message) {
         .toLocaleString([], { hour12: true, timeZone: 'America/New_York' })
         .replace(', ', ' ')
         .split(' ');
-    timeStamp[1] +=
-        ':' + dt.getMilliseconds().toString().padStart(3, '0') + 'ms';
+    timeStamp[1] += ':' + dt.getMilliseconds().toString().padStart(3, '0') + 'ms';
     timeStamp = timeStamp.join(' ');
     console.log(`[${timeStamp}][${logger}] ${message}`);
 }
 
 function random_string(length = 7) {
     var result = [];
-    var characters =
-        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     var charactersLength = characters.length;
     for (var i = 0; i < length; i++) {
-        result.push(
-            characters.charAt(Math.floor(Math.random() * charactersLength))
-        );
+        result.push(characters.charAt(Math.floor(Math.random() * charactersLength)));
     }
     return result.join('');
 }
@@ -30,36 +26,22 @@ function assert_log(group, target, assertion) {
             log(group, 'Verified ' + target);
         } else {
             throw new Error(
-                'Failed To Verify ' +
-                    target +
-                    ' @ ' +
-                    group +
-                    ' -> ' +
-                    assertion.toString()
+                'Failed To Verify ' + target + ' @ ' + group + ' -> ' + assertion.toString()
             );
         }
     } catch (error) {
         console.log(error);
         throw new Error(
-            'Failed To Verify ' +
-                target +
-                ' @ ' +
-                group +
-                ' -> ' +
-                assertion.toString()
+            'Failed To Verify ' + target + ' @ ' + group + ' -> ' + assertion.toString()
         );
     }
 }
 
 function async_for_each(items, handler, cursor = 0, final) {
     if (final == undefined)
-        return new Promise((resolve, reject) =>
-            async_for_each(items, handler, cursor, resolve)
-        );
+        return new Promise((resolve, reject) => async_for_each(items, handler, cursor, resolve));
     if (cursor < items.length)
-        return handler(items[cursor], () =>
-            async_for_each(items, handler, cursor + 1, final)
-        );
+        return handler(items[cursor], () => async_for_each(items, handler, cursor + 1, final));
     return final(); // Resolve master promise
 }
 
@@ -68,4 +50,5 @@ module.exports = {
     random_string: random_string,
     assert_log: assert_log,
     async_for_each: async_for_each,
+    async_wait: async_wait,
 };
