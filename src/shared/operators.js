d384fb6a773f146177cc53ce2e85887abe6da47b
kartikk221 <kartikk221@yahoo.com>
2021-09-10 16:18:54 -0400

    FIxed parse_path_parameters indexing due to uWebsockets change

--- src/shared/operators.js
+++ src/shared/operators.js
@@ -7,12 +7,15 @@
 
 function parse_path_parameters(pattern) {
     let results = [];
+    let counter = 0;
     if (pattern.indexOf('/:') > -1) {
         let chunks = pattern.split('/').filter((chunk) => chunk.length > 0);
         for (let index = 0; index < chunks.length; index++) {
             let current = chunks[index];
-            if (current.startsWith(':') && current.length > 2)
-                results.push([current.substring(1), index]);
+            if (current.startsWith(':') && current.length > 2) {
+                results.push([current.substring(1), counter]);
+                counter++;
+            }
         }
     }
     return results;
