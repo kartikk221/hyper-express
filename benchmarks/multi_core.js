2b6a0941c5cadf4fd5fb92d85c4099787e471488
kartikk221 <kartikk221@yahoo.com>
2021-10-14 22:19:21 +0000

    Improved Benchmarks & Added NanoExpress

--- benchmarks/multi_core.js
+++ benchmarks/multi_core.js
@@ -1,7 +1,6 @@
-let { port_range_start } = require('./configuration.json');
-const PID = process.pid;
 const group = 'MULTI_CORE_BENCHMARK';
 const cluster = require('cluster');
+const startup = require('./startup.js');
 
 function log(logger = 'SYSTEM', message) {
     let dt = new Date();
@@ -21,45 +20,5 @@ if (cluster.isMaster) {
     // Fork workers up to numCPUs
     for (let i = 0; i < numCPUs; i++) cluster.fork();
 } else {
-    const HyperExpress_Server = require('./setup/HyperExpress.js');
-    const Express_Server = require('./setup/Express.js');
-    const Fastify_Server = require('./setup/Fastify.js');
-    const uWebsockets_Server = require('./setup/uWebsockets.js');
-
-    // Start all server instances on a range of ports from port_range_start
-    (async () => {
-        log(group, 'Launching Single Core Listeners For Webservers...');
-
-        // Benchmark uWebsockets Server
-        log(
-            group,
-            'Starting uWebsockets Test HTTP Server On Port: ' + port_range_start + ' & PID: ' + PID
-        );
-        await uWebsockets_Server.listen(port_range_start, () => {});
-        port_range_start++;
-
-        // Benchmark HyperExpress Server
-        log(
-            group,
-            'Starting HyperExpress Test HTTP Server On Port: ' + port_range_start + ' & PID: ' + PID
-        );
-        await HyperExpress_Server.listen(port_range_start);
-        port_range_start++;
-
-        // Benchmark Fastify Server
-        log(
-            group,
-            'Starting Fastify Test HTTP Server On Port: ' + port_range_start + ' & PID: ' + PID
-        );
-        await Fastify_Server.listen(port_range_start, '0.0.0.0');
-        port_range_start++;
-
-        // Benchmark Express Server
-        log(
-            group,
-            'Starting Express Test HTTP Server On Port: ' + port_range_start + ' & PID: ' + PID
-        );
-        await Express_Server.listen(port_range_start);
-        port_range_start++;
-    })();
+    startup();
 }
--- benchmarks/single_core.js
+++ benchmarks/single_core.js
@@ -1,9 +1,5 @@
 const group = 'SINGLE_CORE_BENCHMARK';
-let { port_range_start } = require('./configuration.json');
-const HyperExpress_Server = require('./setup/HyperExpress.js');
-const Express_Server = require('./setup/Express.js');
-const Fastify_Server = require('./setup/Fastify.js');
-const uWebsockets_Server = require('./setup/uWebsockets.js');
+const startup = require('./startup.js');
 
 function log(logger = 'SYSTEM', message) {
     let dt = new Date();
@@ -16,27 +12,7 @@ function log(logger = 'SYSTEM', message) {
     console.log(`[${timeStamp}][${logger}] ${message}`);
 }
 
-// Start all server instances on a range of ports from port_range_start
 (async () => {
     log(group, 'Launching Single Core Listeners For Webservers...');
-
-    // Benchmark uWebsockets Server
-    log(group, 'Starting uWebsockets Test HTTP Server On Port: ' + port_range_start);
-    await uWebsockets_Server.listen(port_range_start, () => {});
-    port_range_start++;
-
-    // Benchmark HyperExpress Server
-    log(group, 'Starting HyperExpress Test HTTP Server On Port: ' + port_range_start);
-    await HyperExpress_Server.listen(port_range_start);
-    port_range_start++;
-
-    // Benchmark Fastify Server
-    log(group, 'Starting Fastify Test HTTP Server On Port: ' + port_range_start);
-    await Fastify_Server.listen(port_range_start, '0.0.0.0');
-    port_range_start++;
-
-    // Benchmark Express Server
-    log(group, 'Starting Express Test HTTP Server On Port: ' + port_range_start);
-    await Express_Server.listen(port_range_start);
-    port_range_start++;
+    startup();
 })();
