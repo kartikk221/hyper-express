2d45f1cacdbab644b0bbae8964a0bb92985ca09e
kartikk221 <kartikk221@yahoo.com>
2022-03-17 19:42:54 -0400

    Fixed Test Scenario To Use Dynamic Test File Length

--- tests/components/features/LiveFile.js
+++ tests/components/features/LiveFile.js
@@ -1,18 +1,24 @@
 const { assert_log } = require('../../scripts/operators.js');
 const { fetch, server } = require('../../configuration.js');
 const { TEST_SERVER } = require('../Server.js');
+const fs = require('fs');
 const endpoint = '/tests/response/send-file';
 const endpoint_url = server.base + endpoint;
+const test_file_path = './content/test.html';
 
 // Create Backend HTTP Route
 TEST_SERVER.get(endpoint, async (request, response) => {
     // We purposely delay 100ms so cached vs. uncached does not rely too much on system disk
-    return response.download('./content/test.html', 'something.html');
+    return response.download(test_file_path, 'something.html');
 });
 
 async function test_livefile_object() {
     let group = 'RESPONSE';
     let candidate = 'HyperExpress.Response';
+
+    // Read the test file into memory
+    const test_file_string = fs.readFileSync(test_file_path).toString();
+    console.log(test_file_string.length);
 
     // Perform fetch request
     const response = await fetch(endpoint_url);
@@ -22,8 +28,14 @@ async function test_livefile_object() {
     const headers = response.headers.raw();
     const content_type = headers['content-type'];
     const content_length = headers['content-length'];
+    console.log(body);
+    console.log(content_type, content_length, body.length);
     assert_log(group, candidate + '.file()', () => {
-        return content_type == 'text/html' && content_length == '120' && body.length == 120;
+        return (
+            content_type == 'text/html' &&
+            content_length == test_file_string.length.toString() &&
+            body.length == test_file_string.length
+        );
     });
 
     // Test Content-Disposition header to validate .attachment()
