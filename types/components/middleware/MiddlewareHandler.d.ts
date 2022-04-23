b0a1b1c6c30ab4bb98bbe17fd5026c0dc5bc80be
Randy Lebeau <randylebeau@gmail.com>
2022-04-22 21:30:13 -0700

    🐞 Fix middleware types.

--- types/components/middleware/MiddlewareHandler.d.ts
+++ types/components/middleware/MiddlewareHandler.d.ts
@@ -1,4 +1,6 @@
 import { MiddlewareNext } from './MiddlewareNext';
+import { Request } from '../http/Request';
+import { Response } from '../http/Response';
 
 export type MiddlewarePromise = Promise<Error|void> | void;
-export type MiddlewareHandler = (request: Request, response: Response, next?: MiddlewareNext) => MiddlewarePromise;
+export type MiddlewareHandler = (request: Request, response: Response, next: MiddlewareNext) => MiddlewarePromise;
