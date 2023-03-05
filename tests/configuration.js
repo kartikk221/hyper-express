1e9242ebfab72541b1d93f61482a733cb1146ac4
kartikk221 <kartikk221@yahoo.com>
2023-03-05 12:43:57 -0500

    Force tests to not do connection pooling to prevent socket hang up errors

--- tests/configuration.js
+++ tests/configuration.js
@@ -1,11 +1,18 @@
+const http = require('http');
 const fetch = require('node-fetch');
 const Websocket = require('ws');
 const EventSource = require('eventsource');
 const HyperExpress = require('../index.js');
 const AbortController = require('abort-controller');
 
+const patchedFetch = (url, options = {}) => {
+    // Use a different http agent for each request to prevent connection pooling
+    options.agent = new http.Agent({ keepAlive: false });
+    return fetch(url, options);
+};
+
 module.exports = {
-    fetch,
+    fetch: patchedFetch,
     Websocket,
     EventSource,
     HyperExpress,
