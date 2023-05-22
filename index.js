3e1438ab63f16151edcd8e7d3d14efee7132df21
kartikk221 <kartikk221@yahoo.com>
2023-05-22 00:54:09 -0400

    Added a way to opt out of Alien UWS and updated DOCS

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict';
 // Use uWebsockets.js new alien mode in which uWebsockets.js runs on a separate event-loop from Node.js allowing for better performance
 // This mode seems to only be supported on Linux at the moment
-const supported_platforms = ['linux', 'darwin'];
-if (supported_platforms.includes(process.platform) && !process.env['ALIEN_UWS']) process.env['ALIEN_UWS'] = 1;
+const supported_platforms = ['linux'];
+const alien_mode_allowed = process.env['ALIEN_UWS_FORBIDDEN'] === undefined;
+if (supported_platforms.includes(process.platform) && alien_mode_allowed) process.env['ALIEN_UWS'] = 1;
 
 // Load uWebSockets.js and fundamental Server/Router classes
 const uWebsockets = require('uWebSockets.js');
