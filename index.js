d071d2eea0371495351a8c17a1a9182164f4261a
kartikk221 <kartikk221@yahoo.com>
2023-05-23 17:17:18 -0400

    Disables the uWebsockets.js version header

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const uWebsockets = require('uWebSockets.js');
 const Server = require('./src/components/Server.js');
 const Router = require('./src/components/router/Router.js');
 
+// Disable the uWebsockets.js version header
+try {
+    uWebsockets._cfg('999999990007');
+} catch (error) {}
+
 // Expose Server and Router classes along with uWebSockets.js constants
 module.exports = {
     Server,
