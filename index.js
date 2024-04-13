412a5a65e07e0a1d02fd4a31df3bffbca814b7a6
DadiBit <davide.bassi314@gmail.com>
2024-04-13 09:33:37 +0200

    fix: reflect .d.ts exports in index.js
    See: https://github.com/kartikk221/hyper-express/issues/249

--- index.js
+++ index.js
@@ -4,6 +4,12 @@
 const uWebsockets = require('uWebSockets.js');
 const Server = require('./src/components/Server.js');
 const Router = require('./src/components/router/Router.js');
+const Request = require('./src/components/http/Request.js');
+const Response = require('./src/components/http/Response.js');
+const LiveFile = require('./src/components/plugins/LiveFile.js');
+const MultipartField = require('./src/components/plugins/MultipartField.js');
+const SSEventStream = require('./src/components/plugins/SSEventStream.js');
+const Websocket = require('./src/components/ws/Websocket.js');
 
 // Disable the uWebsockets.js version header if not specified to be kept
 if (!process.env['KEEP_UWS_HEADER']) {
@@ -14,7 +20,13 @@ if (!process.env['KEEP_UWS_HEADER']) {
 
 // Expose Server and Router classes along with uWebSockets.js constants
 module.exports = {
+    compressors: uWebsockets,
     Server,
     Router,
-    compressors: uWebsockets,
+    Request,
+    Response,
+    LiveFile,
+    MultipartField,
+    SSEventStream,
+    Websocket,
 };
